b2e870b0a93913371441d0221c6de3c056702eb5
dgistando <davidgistando@gmail.com>
2016-06-30 23:42:56 -0700

    the layout has changes totally

--- SSR_Inventory/src/inventory/Inventory_Controller.java
+++ SSR_Inventory/src/inventory/Inventory_Controller.java
@@ -2,9 +2,12 @@ package inventory;
 
 import com.sun.javafx.scene.control.skin.ButtonBarSkin;
 import com.sun.javafx.scene.control.skin.ContextMenuContent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -22,12 +25,29 @@ public class Inventory_Controller implements Initializable{
 
     @FXML
     TabPane TPinventory;
+    @FXML
+    ContextMenu CMinventory;
+    @FXML
+    Tab CMTab;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         assert TPinventory != null : "tabpane TPinventory doesn't exist";
+        assert CMinventory != null : "tabpane CMinventory doesn't exist";
 
-        Tab tab = new Tab();
+
+
+        TPinventory.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(event.getButton() == MouseButton.PRIMARY){
+                    System.out.println("you clicked this left");
+                }
+                //System.out.println("you clicked menu");
+            }
+        });
+
+        /*Tab tab = new Tab();
         HBox box = new HBox();
         ContextMenu cm = new ContextMenu();
         cm.getItems().add(new MenuItem("some text", new Circle(5.0,Color.RED)));
@@ -36,6 +56,6 @@ public class Inventory_Controller implements Initializable{
         tab.setContent(box);
         //makes the image on the tab(upsidedown triangle)
         //tab.setGraphic();
-        TPinventory.getTabs().add(tab);
+        TPinventory.getTabs().add(tab);*/
     }
 }
