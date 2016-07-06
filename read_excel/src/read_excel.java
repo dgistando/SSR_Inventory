3d411bf9d1d9e4ef19cdac183b5d938d6616d845
dgistando <davidgistando@gmail.com>
2016-07-06 15:45:01 -0700

    added project with how to read excel

---
+++ read_excel/src/read_excel.java
@@ -0,0 +1,72 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * Created by SSR on 7/6/2016.
+ */
+public class read_excel {
+
+    public static void main(String[] args) throws IOException{
+        File file = new File("src/test.xls");
+        FileInputStream inputStream = new FileInputStream(file);
+
+        Workbook workbook = new HSSFWorkbook(inputStream);
+        Sheet sheet = workbook.getSheetAt(0);
+        Iterator<Row> rowIterator = sheet.iterator();
+
+        int i = -9999;
+
+        while(rowIterator.hasNext()){
+            Row nextRow = rowIterator.next();
+            Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+
+            while (cellIterator.hasNext()){
+                Cell cell = cellIterator.next();
+
+
+                while(cell.getColumnIndex()-1 > i && i!=-9999){
+                    System.out.print(" blank ");
+                    i++;
+                }
+
+               // System.out.print(cell.getCellType());
+                switch (cell.getCellType()){
+                    case Cell.CELL_TYPE_NUMERIC:
+                        System.out.print(cell.getNumericCellValue());
+                        break;
+                    case Cell.CELL_TYPE_BOOLEAN:
+                        System.out.print(cell.getBooleanCellValue());
+                        break;
+                    case Cell.CELL_TYPE_FORMULA:
+                        System.out.print(cell.getCellFormula());
+                        break;
+
+                    case Cell.CELL_TYPE_STRING:
+                        System.out.print(cell.getRichStringCellValue());
+                        break;
+                    default:
+                }
+
+
+
+                System.out.print(" ");
+                i = cell.getColumnIndex();
+            }
+            System.out.println("");
+        }
+
+    }
+
+}
