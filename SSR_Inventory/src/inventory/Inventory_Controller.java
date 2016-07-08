a74c1e4b408de84a1bfa8af7e6f2f882eddee569
dgistando <davidgistando@gmail.com>
2016-07-07 23:34:34 -0700

    added toolbar

--- SSR_Inventory/src/inventory/Inventory_Controller.java
+++ SSR_Inventory/src/inventory/Inventory_Controller.java
@@ -2,12 +2,15 @@ package inventory;
 
 import com.sun.javafx.scene.control.skin.ButtonBarSkin;
 import com.sun.javafx.scene.control.skin.ContextMenuContent;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -29,15 +32,23 @@ public class Inventory_Controller implements Initializable{
     ContextMenu CMinventory;
     @FXML
     Tab CMTab;
+    @FXML
+    AnchorPane pane;
+    @FXML
+    Label label;
+    @FXML
+    Button button;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         assert TPinventory != null : "tabpane TPinventory doesn't exist";
         assert CMinventory != null : "tabpane CMinventory doesn't exist";
+        assert CMTab != null : "the tab doesn't  exist";
+        assert pane != null : "";
+        assert button != null : "dd";
+        assert label != null :" dd";
 
-
-
-        TPinventory.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+        /*TPinventory.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 if(event.getButton() == MouseButton.PRIMARY){
@@ -45,8 +56,22 @@ public class Inventory_Controller implements Initializable{
                 }
                 //System.out.println("you clicked menu");
             }
-        });
+        });*/
 
+        TPinventory.getSelectionModel().selectedItemProperty().addListener(
+                new ChangeListener<Tab>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Tab> observable, Tab oldValue, Tab newValue) {
+                        System.out.println("tab changed");
+                        pane.setVisible(false);
+                        pane.setVisible(true);
+                    }
+                }
+        );
+
+        button.setOnAction(event -> {
+            label.setText("feiwfjwiefj epijrpi4jpi2jpi \n oneunfeowfu \n enufnewfoewunfoeufoeu \ndepiwfjpijwpiwjepfij");
+        });
         /*Tab tab = new Tab();
         HBox box = new HBox();
         ContextMenu cm = new ContextMenu();
